b99d1a51bb08a64df965f504da29c549b8e53bf8
nsm4421 <62060741+nsm4421@users.noreply.github.com>
2021-08-15 00:16:33 +0900

    Flask Basic
    https://blog.naver.com/nsm4421/222470455609

---
+++ Flask/Basic.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Index Page
+@app.route("/")     
+def index():
+    return "<h1> Hello World </h1>"
+
+# test page
+@app.route("/test")     
+def test():
+    return "<h1> Test Page </h1>"
+
+# 주소 할당하기
+@app.route("/user/<string:username>")     
+def Greeting(username):
+    return f"<h1> Hi {username} </h1>"
+
+# Dictionary ---> Json
+@app.route("/jsonTest/<string:username>", methods=['GET'])     
+def JsonTest(username):
+    if len(username)<3:
+        return "유저 이름 좀 이상함"
+    else:
+        userDict = {"성" : username[0], \
+            '이름' : username[1:]}
+        return jsonify(userDict)
+
+
+if __name__=='__main__':
+    app.debug = True    # debug mode
+    app.config['JSON_AS_ASCII'] = False
+    app.run(host="0.0.0.0", port = "8080")
