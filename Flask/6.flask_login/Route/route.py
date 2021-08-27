5ab22eb0970092240ec8746f34eb14e969639908
nsm4421 <62060741+nsm4421@users.noreply.github.com>
2021-08-28 01:51:04 +0900

    디자인 수정

--- Flask/6.flask_login/Route/route.py
+++ Flask/6.flask_login/Route/route.py
@@ -1,46 +1,53 @@
-from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for
+from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for, flash
 from flask_login import login_user, current_user, logout_user
 import datetime
-
-
 from Control.manage_user import User
-
 myApp = Blueprint('myApp', __name__)
 
 @myApp.route('/')
+@myApp.route('/index')
 def index():
     return render_template('index.html')
-
+#=================== 로그인 ===================#
 @myApp.route('/login')
 def login():
+    if current_user.is_authenticated:
+        flash("이미 로그인 됐는데?")
+        return redirect(url_for('myApp.main'))
     return render_template('login.html')
-
 @myApp.route('/loginAction', methods=['POST'])
 def loginAction():
     user_id_given = request.form['user_id']
     user_pw_given = request.form['user_pw']
-
     user = User.get(user_id_given)    
     if user==None:  # 존재하지 않는 아이디
+        flash("존재하지 않는 아이디임")
         return redirect(url_for('myApp.login'))
     elif user_pw_given != user.pw:  # 비밀번호 오류
+        flash("비밀번호 틀림")
         return redirect(url_for('myApp.login'))
     elif user_pw_given == user.pw:  # 로그인 성공
         login_user(user)
-        return f"{user.id}님 로그인 성공"
+        flash("로그인 성공")
+        return redirect(url_for('myApp.main'))
     else:   # DB 오류
-        return redirect(url_for('myApp.login'))
-
+        flash("DB 오류")
+        return redirect(url_for('myApp.index'))
+#=================== 로그아웃 ===================#
 @myApp.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('myApp.login'))
-
+def logout():       
+    if current_user.is_authenticated:
+        logout_user()
+        return redirect(url_for('myApp.index'))
+    flash("아직 로그인도 안했는데 뭔 로그아웃?")
+    return redirect(url_for('myApp.login')) 
+#=================== 회원가입 ===================#
 @myApp.route('/register')
 def register():
+    if current_user.is_authenticated:
+        flash("이미 로그인 되어 있습니다.")
+        return redirect(url_for('myApp.index'))
     return render_template('signUp.html')
-
-
 @myApp.route('/registerAction', methods=['POST'])
 def registerAction():
     user_id = request.form['user_id']
@@ -52,18 +59,15 @@ def registerAction():
     if result==1:  # 회원가입 성공
         return redirect(url_for('myApp.login')) #  로그인 페이지로
     else:   # DB 오류
-        return "<h1> 회원가입 오류 </h1>"
-
-# 회원탈퇴
+        flash("회원가입 실패~")
+        return redirect(url_for('myApp.register'))
+#=================== 회원탈퇴 ===================#
 @myApp.route('/delete')
 def delete():
     if current_user.is_authenticated:
         return render_template('delete.html')
-        
     else:
-        return redirect(url_for('myApp.login'))
-        
-
+        return redirect(url_for('myApp.index'))
 
 @myApp.route('/deleteAction', methods = ['GET','POST'])
 def deleteAction():
@@ -72,13 +76,9 @@ def deleteAction():
         logout_user()
         if result==1:  # 회원탈퇴 성공
             return redirect(url_for('myApp.register')) #  회원가입 페이지로
-    return "<h1> 오류 </h1>"
-
-
-
+        return redirect(url_for('myApp.index'))
+    return redirect(url_for('myApp.index'))
+#=================== 메인페이지 ===================#
 @myApp.route('/main')
 def main():
-    if current_user.is_authenticated:
-        return render_template('main.html', user_id=current_user.user_id)
-    else:
-        return redirect(url_for('myApp.login'))
+    return render_template('main.html')
