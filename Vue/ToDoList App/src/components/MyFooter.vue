94ece0a603ef79db8d20f830a852f09b8651181d
nsm4421 <62060741+nsm4421@users.noreply.github.com>
2021-09-20 18:01:09 +0900

    To Do List App

---
+++ Vue/ToDoList App/src/components/MyFooter.vue
@@ -0,0 +1,23 @@
+<template>
+    <span @click="clearAll">
+        <p>전부 삭제하기</p>
+    </span>
+    
+</template> 
+
+<script>
+export default{
+    methods: {
+        clearAll () {
+            // 이벤트 발생
+            this.$emit('clearAll');
+        }
+    }
+}
+</script>
+
+<style scoped>
+p{
+    background: lightpink;
+}
+</style>
---
+++ Vue/ToDoList App/src/components/MyHeader.vue
@@ -0,0 +1,26 @@
+<template>
+<header>
+    <h1>할일 목록 만들기</h1>
+</header>   
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+h1 {
+    color: white;
+}
+header {
+    /* 가운데 정렬 */
+    text-align : center;    
+    /* 배경 색깔 */
+    background: cadetblue;
+}
+.shadow {
+    box-shadow: teal;
+}
+</style>
