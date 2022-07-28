dcf1d5dc0a03126d6de11900d3b36a1b99072869
karma <nsm4421@naver.com>
2022-07-28 14:42:27 +0900

    View(article)

--- src/main/java/com/karma/karmaboard/KarmaApplication.java
+++ src/main/java/com/karma/karmaboard/KarmaApplication.java
@@ -1,9 +1,12 @@
 package com.karma.karmaboard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class KarmaApplication {
 
     public static void main(String[] args) {
