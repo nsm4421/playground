fc2d0c2109b5503007f1ca5fe076918d314a6ab9
karma <nsm4421@naver.com>
2022-09-09 20:45:08 +0900

    View 만들기 #1 - Navigation바
    React Router & Bootstrap

---
+++ karma/front-end/src/components/MyNav.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+import { Navbar, Container, Nav} from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom';
+
+
+const links = [
+    {
+        LABEL:"Home",
+        HREF:"/"
+    },
+    {
+        LABEL:"PAGE1",
+        HREF:"/page1"
+    },
+    {
+        LABEL:"PAGE2",
+        HREF:"/page2"
+    },
+    {
+        LABEL:"PAGE3",
+        HREF:"/page3"
+    },
+    {
+        LABEL:"PAGE4",
+        HREF:"/page4"
+    },
+  ]
+  
+
+const MyNav = () => {
+
+    const [selected, setSelected] = useState(0);
+    const navigator = useNavigate();
+
+    const handleClick = (i) => (e)=>{
+        e.preventDefault();
+        setSelected(i);
+        navigator(links[i].HREF);
+    }
+
+    return (
+        <div>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+            <Navbar.Brand href="#" className='text-warning'>Karma</Navbar.Brand>
+            <Nav className="me-auto">
+                {
+                    links.map((l, i)=>{
+                        return (
+                            <Nav.Link 
+                                onClick={handleClick(i)}
+                                className={i === selected ? 'text-white' : 'text-secondary'}
+                                key={i} href={l.HREF}>{l.LABEL}
+                            </Nav.Link>
+                        )
+                    })
+                }
+            </Nav>
+            </Container>
+        </Navbar>
+        </div>
+  )
+}
+
+export default MyNav
