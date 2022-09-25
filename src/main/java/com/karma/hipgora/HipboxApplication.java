1e428b34fe838d11d54e0cb5c0ac5f9d61a72d16
karma <nsm4421@naver.com>
2022-09-25 15:14:12 +0900

    Project 생성
    hiphop 작업물을 공유할 수 있는 Spring Project

---
+++ src/main/java/com/karma/hipgora/HipboxApplication.java
@@ -0,0 +1,13 @@
+package com.karma.hipgora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HipboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karma/hipgora/HipboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karma.hipgora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
