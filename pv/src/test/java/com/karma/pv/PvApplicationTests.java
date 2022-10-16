0d6454829fb428435bb5d47caf65ef51aeba54a2
karma <nsm4421@naver.com>
2022-10-16 21:01:13 +0900

    PV 산출 프로젝트 생성

---
+++ pv/src/test/java/com/karma/pv/PvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karma.pv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
