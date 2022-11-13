0f2591d33d5ac6a2b218ce13237b074459ab365d
nsm <nsm4421@naver.com>
2022-11-13 15:04:39 +0900

    Repository Test
    CRUD Test

---
+++ src/main/java/com/karma/board/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.karma.board.repository;
+
+import com.karma.board.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
---
+++ src/main/java/com/karma/board/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.karma.board.repository;
+
+import com.karma.board.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
