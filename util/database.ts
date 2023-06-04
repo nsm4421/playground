c999e079dbd31ef4a6fd619a1cf068f3d3153a0c
nsm <nsm4421@naver.com>
2023-06-04 13:51:39 +0900

    TEST Database Connection
    Test for getting data from mongo database

---
+++ util/database.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+const uri = process.env.MONGO_URI;
+let connectDB:Promise<MongoClient>
+if (!uri) throw new Error()
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(uri).connect()
+  }
+  connectDB = global._mongo
+} else {
+  connectDB = new MongoClient(uri).connect()
+}
+export { connectDB }
