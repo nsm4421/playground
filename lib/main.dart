c57903c00fd6007086cbfb918cc8aa5409b0943b
nsm <nsm4421@naver.com>
2023-07-22 17:56:38 +0900

    이미지 파일 다운로드/라이브러리 설치
    - 알림 화면에 적용할 이미지 다우놀드
    - timeago 라이브러리 설치 및 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'common/data/preference/app_preferences.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 void main() async {
   final bindings = WidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await AppPreferences.init();
+
+  // timeago 라이브러리 한국어로 세팅
+  timeago.setLocaleMessages('ko', timeago.KoMessages());
 
   runApp(EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ko')],
