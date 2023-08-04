cd3e50e0dda2eb99f01bfdf8fec9873d0ca0497d
nsm <nsm4421@naver.com>
2023-08-04 20:39:07 +0900

    CHORE Firebase Setting
    http://blog.naver.com/nsm4421?Redirect=Update&logNo=223175047148

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sns/firebase_options.dart';
 import 'package:flutter_sns/screen/s_app.dart';
 import 'package:flutter_sns/util/init_binding.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
