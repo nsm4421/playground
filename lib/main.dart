f5a8f2cd58cb84d228729c3a9b1b64b57cf7556e
nsm <nsm4421@gmail.com>
2023-09-03 14:39:56 +0900

    REBASE Init Project
    뭔가 맘에 안듬
    다시 만들어야지

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
-import 'package:chat_app/common/routes/routes.dart';
-import 'package:chat_app/common/theme/themes.dart';
-import 'package:chat_app/controller/auth_controller.dart';
-import 'package:chat_app/screen/util/s_error.dart';
 import 'package:chat_app/screen/s_home.dart';
-import 'package:chat_app/screen/util/s_loading.dart';
-import 'package:chat_app/screen/s_welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
@@ -15,30 +8,21 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const MyApp());
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final theme = Themes();
+  Widget build(BuildContext context) {
     return MaterialApp(
-        onGenerateRoute: CustomRoutes.onGeneralRoute,
-        debugShowCheckedModeBanner: false,
-        title: "Chat App",
-        themeMode: ThemeMode.system,
-        theme: theme.lightTheme,
-        darkTheme: theme.darkTheme,
-        home: ref.watch(currentAuthProvider).when(
-            // 로그인 안된 경우 WelcomeScreen, 로그인한 경우 HomeScreen
-            data: (user) =>
-                user == null ? const WelcomeScreen() : const HomeScreen(),
-            // 에러 발생시
-            error: (error, trace) =>
-                ErrorScreen(errorMessage: error.toString()),
-            // 로딩 중
-            loading: () => const LoadingScreen()));
+      debugShowCheckedModeBanner: false,
+      title: "Chat App",
+      themeMode: ThemeMode.system,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: HomeScreen(),
+    );
   }
 }
