027ed5d7c349801f45af65c098aaa2d7eacfb196
nsm <nsm4421@naver.com>
2023-09-09 16:35:00 +0900

    FEAT Skeleton Code For Auth
    Clerk 라이브러리를 사용해 google, github 소셜 로그인 페이지 작성

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs/app-beta";
+
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>      
+      <h1>Home</h1>  
+      <UserButton afterSignOutUrl="/"/>    
     </main>
   )
 }
