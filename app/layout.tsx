bc4813986adfe98b6c60ac8c480676796fa00182
Karma <nsm4421@naver.com>
2024-05-10 17:59:52 +0900

    Project Setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lobster } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/components/theme-provider";
+
+const font = Lobster({
+  weight: "400",
+  subsets: ["latin"]
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <body className={font.className}>{children}</body>
+      </ThemeProvider>
     </html>
   );
 }
