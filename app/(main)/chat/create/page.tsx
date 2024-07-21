4d06fda1d795aed9537f597645bbfc9ef6d6f2cc
Karma <nsm4421@naver.com>
2024-07-21 14:07:43 +0900

    init project
    flutter 프로젝트로 변경함

--- app/(main)/chat/create/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function CreateChatPage() {
-  return (
-    <main className="container">
-      <h1>Create CHAT</h1>
-    </main>
-  );
-}
--- lib/constant/routes.ts
+++
@@ -1,8 +0,0 @@
-export enum Routes {
-    index = "/",
-    signUp = "/sign-up",
-    signIn = "/sign-in",
-    chat = "/chat",
-    createChat = "/chat/create",
-    myPage = "/my-page",
-}
