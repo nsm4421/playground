45930bba2c26cbc3a4c6c879a4a7ac1b3fa493bb
Karma <nsm4421@naver.com>
2024-09-12 00:20:53 +0900

    [FEAT] Create Feed State
    피드 작성 시 상태 관리를 위해 cubit 생성

---
+++ lib/create_media/presentation/bloc/create_feed/create_feed.state.dart
@@ -0,0 +1,58 @@
+part of 'create_feed.cubit.dart';
+
+class CreateFeedState extends BaseState {
+  final String caption;
+  final List<String> hashtags;
+  final List<AssetPathEntity> albums;
+  final AssetPathEntity? currentAlbum;
+  final List<AssetEntity> assets;
+  final AssetEntity? currentAsset;
+  final bool isAuth;
+  final bool isEnd;
+
+  CreateFeedState({
+    required super.id,
+    super.status = Status.initial,
+    super.step = CreateStep.selectMedia,
+    super.media,
+    this.caption = '',
+    required this.hashtags,
+    super.errorMessage = '',
+    required this.albums,
+    this.currentAlbum,
+    required this.assets,
+    this.currentAsset,
+    this.isAuth = false,
+    this.isEnd = false,
+  });
+
+  @override
+  CreateFeedState copyWith({
+    Status? status,
+    CreateStep? step,
+    File? media,
+    String? caption,
+    List<String>? hashtags,
+    String? errorMessage,
+    List<AssetPathEntity>? albums,
+    AssetPathEntity? currentAlbum,
+    List<AssetEntity>? assets,
+    AssetEntity? currentAsset,
+    bool? isAuth,
+    bool? isEnd,
+  }) =>
+      CreateFeedState(
+          id: id,
+          status: status ?? this.status,
+          step: step ?? this.step,
+          media: media ?? this.media,
+          caption: caption ?? this.caption,
+          hashtags: hashtags ?? this.hashtags,
+          errorMessage: errorMessage ?? this.errorMessage,
+          albums: albums ?? this.albums,
+          currentAlbum: currentAlbum ?? this.currentAlbum,
+          assets: assets ?? this.assets,
+          currentAsset: currentAsset ?? this.currentAsset,
+          isAuth: isAuth ?? this.isAuth,
+          isEnd: isEnd ?? this.isEnd);
+}
