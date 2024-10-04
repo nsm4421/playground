77f93e5fb59cb465a511e90db75505ed77d66fb0
Karma <nsm4421@naver.com>
2024-10-05 00:57:58 +0900

    [ENHENCE] Inject Enviroment Varibale
    Envied라이브러리를 사용해 환경변수 주입

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,15 @@ import 'package:travel/presentation/bloc/auth/authentication.bloc.dart';
 import 'package:travel/presentation/route/router.dart';
 
 import 'core/di/dependency_injection.dart';
+import 'core/env/env.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // TODO : local에서 돌리고 있어서 따로 의존성 주입하지 않고, 키값을 적음.
-  // 이후에 flutter dotenv나 envy라이브러리를 사용해 의존성 주입하는 방식으로 수정하기
   await Supabase.initialize(
-      url: 'http://127.0.0.1:54321',
-      anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0');
+      url: Env.supabaseUrl,
+      anonKey: Env.supabaseAnonKey); // supabase client 초기화
+
   configureDependencies(); // 의존성 주입
 
   runApp(const MainApp());
@@ -30,7 +29,11 @@ class MainApp extends StatelessWidget {
       child: MaterialApp.router(
         title: 'Traveler',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple,
+              primary: Colors.blue,
+              secondary: Colors.teal,
+              tertiary: Colors.blueGrey),
           useMaterial3: true,
         ),
         routerConfig: routerConfig,
