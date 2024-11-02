2b84f6ac180e31dee8308fb8b7514c957a82876a
Karma <nsm4421@naver.com>
2024-11-02 17:46:12 +0900

    Custom Theme 정의하기

---
+++ lib/core/theme/palette.theme.dart
@@ -0,0 +1,50 @@
+part of 'theme.dart';
+
+class CustomPalette {
+  static const white = Color(0xFFFFFFFF);
+
+  static const black = Color(0xFF000000);
+
+  static const primary = Color(0xFF5F0080);
+
+  static const onPrimary = Color(0xFFFFFFFF);
+
+  static const secondary = Color(0xFFE86C3F);
+
+  static const onSecondary = Color(0xFFFFFFFF);
+
+  static const error = Color(0xFFFF5645);
+
+  static const onError = Color(0xFFFFFFFF);
+
+  static const surface = Color(0xFFF4F4F4);
+
+  static const surfaceVariant = Color(0xFFD9D9D9);
+
+  static const onSurface = Color(0xFF1E1B1E);
+
+  static const onSurfaceVariant = Color.fromRGBO(25, 25, 35, 0.15);
+
+  static const primaryContainer = Color(0xFF6F1A8C);
+
+  static const inverseSurface = Color.fromRGBO(25, 25, 35, 0.74);
+
+  static const inversePrimary = Color.fromRGBO(189, 118, 255, 0.9);
+
+  static const onInverseSurface = Color(0xFFDDDDDD);
+
+  static const shadow = Color(0xFF000000);
+
+  static const outline = Color(0xFFDDDDDD);
+
+  // extended color
+  static const contentPrimary = Color(0xFF333333);
+
+  static const contentSecondary = Color(0xFF666666);
+
+  static const contentTertiary = Color(0xFF999999);
+
+  static const contentFourth = Color(0xFFB2B2B2);
+
+  static const positive = Color(0xFF74CD7C);
+}
