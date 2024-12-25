78dd486ee7ef3ddc871e4f8f0805864311377222
Karma <nsm4421@naver.com>
2024-12-26 00:52:38 +0900

    [BACKEND] BoilerPlate
    - setup files to connect to postgres database
    - install libraries for authentication

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { UserEntity } from './users/entity/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // TODO : inject secret by .env file
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'karma',
+      password: '1221',
+      database: 'dev_db',
+      autoLoadEntities: true,
+      synchronize: true, // only on dev mode
+      entities: [UserEntity],
+    }),
+
+    UsersModule,
+  ],
 })
 export class AppModule {}
