25002407a3077b77133732b3e40839180b78eac5
Karma <nsm4421@naver.com>
2025-02-17 02:19:49 +0900

    [FEAT] Compose Feed UI
    parallel, intercept routing을 사용해서 피드작성 모달창 구현

---
+++ src/app/(authorized)/compose/feed/page.tsx
@@ -0,0 +1,11 @@
+import ComposeFeedModalPage from "../../@modal/(.)compose/feed/page";
+import HomePage from "../../home/page";
+
+export default function ComposeFeedPage() {
+  return (
+    <>
+      <HomePage />
+      <ComposeFeedModalPage />
+    </>
+  );
+}
